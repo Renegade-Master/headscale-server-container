c8038ebc67007a8e89ab23af92685f69beb7c416
Renegade Master <renegade@renegade-master.com>
2026-07-15 15:32:15 +0100

    Add Listen Port Configuration;

--- run_headscale_server.sh
+++ run_headscale_server.sh
@@ -6,15 +6,20 @@ source "$(dirname $0)/common.sh"
 # Exit any existing Headscale Containers
 podman stop "${name_server}" && podman rm "${name_server}" || true
 
+# Enter a Port to expose
+read -p "Enter the Port that the Server should listen on for new connections [default: 8080]: " server_port
+if [[ ${server_port} == "" ]]; then
+    server_port="8080"
+fi
+
 # Start Headscale server process
 podman run \
   --name="${name_server}" \
   --detach \
   --volume "${data_dir}":"/etc/headscale:Z" \
   --volume "${work_dir}/lib":"/var/lib/headscale:Z" \
-  --publish "8080:8080/tcp" \
+  --publish "${server_port}:8080/tcp" \
   --publish "9090:9090/tcp" \
-  --publish "51820:51820/tcp" \
   --health-cmd "CMD headscale health" \
   --restart "unless-stopped" \
   "${image}" \
