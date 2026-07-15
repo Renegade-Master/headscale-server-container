6cb5a9376016868af488d8a1c337a3fa864bcd38
Renegade Master <renegade@renegade-master.com>
2026-07-15 15:12:47 +0100

    Add File to Generate Keys;

--- get_headscale_auth_key.sh
+++ get_headscale_auth_key.sh
@@ -6,14 +6,13 @@ source "$(pwd)/common.sh"
 # List Users
 $(pwd)/list_headscale_users.sh
 
-# Get User Information
-read -p "Enter the ID for the User to be deleted: " user_id
+# Get User
+read -p "Enter the ID for the User to generate a PreAuth Key for: " user_id
 if [[ ${user_id} == "" ]]; then
     fail_with_error "User ID must not be blank"
 fi
 
-# Delete a User
+# Get PreAuth Key for a User
 podman exec -it \
     "${name_server}" \
-    headscale users destroy \
-        --identifier "${user_id}"
+    headscale preauthkeys create --user "${user_id}"
