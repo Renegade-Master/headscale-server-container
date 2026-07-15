3fcd2a9e4ed06673007916d06f006e305ef3de69
Renegade Master <renegade@renegade-master.com>
2026-07-15 15:39:52 +0100

    Add Node Registration Script;

---
+++ add_headscale_node.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set +x -eu -o pipefail
+
+source "$(pwd)/common.sh"# Get User Information
+
+# Get the Username
+read -p "Enter the Username for the User to be added to a Node: " user_name
+if [[ ${user_name} == "" ]]; then
+    fail_with_error "Username must not be blank"
+fi
+
+# Get the Auth ID
+read -p "Enter the Auth ID for the Node: " auth_id
+if [[ ${auth_id} == "" ]]; then
+    fail_with_error "Auth ID must not be blank"
+fi
+
+# Add a Node to a User
+podman exec -it \
+    "${name_server}" \
+    headscale auth register \
+        --auth-id "${auth_id}"
